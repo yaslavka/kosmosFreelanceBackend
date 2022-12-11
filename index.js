bd97ca69df7cde8fb48fa0f900fa882f08f0c14c
yaslavka <obrubinskii@gmail.com>
2022-12-12 03:30:44 +0500

    ok
    ok

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ const credentials = {
 app.use(cors());
 app.use(express.json());
 app.use("/api/user", express.static(path.resolve(__dirname, "files", "images")));
-app.use("/", express.static(path.resolve(__dirname, "files")));
+app.use("/", express.static(path.resolve(__dirname, "files", "build")));
 app.use(fileUpload({}));
 app.use("/api", router);
 app.use(ErrorHandlingMiddleware);
